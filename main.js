f79e083c4662d89d9c5c8fff7e6d9778ce452ab3
Aline Hamano <98706488+AyuHamano@users.noreply.github.com>
2023-02-07 00:52:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function troca_img(e) {
+    document.querySelector('#phone').src = e; /*pega o elemento phone e faz ele receber a imagem pelo src*/
+}
+//function crem() {
+//    document.querySelector("phone").src = "img/creme.png"; /*pega o elemento phone e faz ele receber a imagem pelo src*/
+//}
+//function graf() {
+//    document.querySelector("phone").src = "img/grafite.png"; /*pega o elemento phone e faz ele receber a imagem pelo //src*/
+//}
